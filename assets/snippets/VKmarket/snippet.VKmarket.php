bc1eba8ac44170e4162f422f8c1df2f49a514164
cgehuzi <mail.cgehuzi@gmail.com>
2019-07-19 15:24:18 +0300

    правит require_once для возможности нескольких вызовов на странице

--- assets/snippets/VKmarket/snippet.VKmarket.php
+++ assets/snippets/VKmarket/snippet.VKmarket.php
@@ -19,50 +19,50 @@ $vk = new VKmarket($access_token, $v);
 switch ($api_method) {
 
     case 'market.add':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.add.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.add.php";
         break;
 
     case 'market.addAlbum':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.addAlbum.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.addAlbum.php";
         break;
 
     case 'market.addToAlbum':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.addToAlbum.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.addToAlbum.php";
         break;
 
     case 'market.delete':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.delete.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.delete.php";
         break;
 
     case 'market.deleteAlbum':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.deleteAlbum.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.deleteAlbum.php";
         break;
 
     case 'market.edit':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.edit.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.edit.php";
         break;
 
     case 'market.editAlbum':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.editAlbum.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.editAlbum.php";
         break;
 
     case 'market.get':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.get.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.get.php";
         break;
 
     case 'market.getAlbums':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.getAlbums.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.getAlbums.php";
         break;
 
     case 'market.getCategories':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.getCategories.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.getCategories.php";
         break;
 
     case 'market.removeFromAlbum':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.removeFromAlbum.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.removeFromAlbum.php";
         break;
 
     case 'market.search':
-        return require_once MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.search.php";
+        return require MODX_BASE_PATH . "assets/snippets/VKmarket/methods/market.search.php";
         break;
 }
