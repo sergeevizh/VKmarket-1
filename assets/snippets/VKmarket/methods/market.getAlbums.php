b631ecfb1ff71040a357c1e0240ae0dc289bc627
cgehuzi <mail.code.is@yandex.ru>
2019-07-25 20:23:56 +0300

    [ market.getAlbums ] - code review

--- assets/snippets/VKmarket/methods/market.getAlbums.php
+++ assets/snippets/VKmarket/methods/market.getAlbums.php
@@ -15,16 +15,43 @@
 & count             |  количество возвращаемых подборок
 ============================================================= */
 
+// Генерируем запрос обязательных параметров
+$request_params = array(
+    'owner_id' => "-$group_id"
+);
+
+// Добавляем к запросу доп. параметры
+if (isset($offset)) {
+    $request_params['offset'] = $offset;
+}
+if (isset($count)) {
+    $request_params['count'] = $count;
+}
+
 // Запрашиваем список подборок
-$request = $vk->getAlbums([
-    'owner_id' => "-$group_id",
-    'offset' => $offset ? $offset : 0,
-    'count' => $count ? $count : 50
-]);
+$request = $vk->request('market.getAlbums', $request_params);
 
 // Если список не получен
 if (!isset($request['count'])) {
-    return $request; // выводим отчёт об ошибке
+    // выводим отчёт об ошибке
+    return $vk->report($response, $request);
 }
 
-return $request; // Выводим отчёт об успешном удалении товара
+// Генерируем отчёт об успехе
+$result = array(
+    'success' => array(
+        'message' => 'Albums received',
+        'response' => $request
+    )
+);
+
+// Добавляем к отчёту доп. параметры
+if (isset($offset)) {
+    $result['success']['request_params']['offset'] = (int) $offset;
+}
+if (isset($count)) {
+    $result['success']['request_params']['count'] = (int) $count;
+}
+
+// Выводим отчёт об успехе
+return $vk->report($response, $result);
