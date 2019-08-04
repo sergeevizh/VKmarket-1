0cfab7e6355238b88d01a150b1bfeea6356aa90b
cgehuzi <mail.code.is@yandex.ru>
2019-08-04 16:43:21 +0300

    [ front ] - change tabs to child

--- assets/modules/VKmarket/module.VKmarket.php
+++ assets/modules/VKmarket/module.VKmarket.php
@@ -17,21 +17,22 @@ if (!isset($template_album))    $errors = $errors . '<li><strong>ID шаблон
 $market = new VKmarket($modx);
 
 // Если есть ошибки
-if ($errors !== "") {
+if ($errors) {
     // Генерируем фронт с отчётом
     $tpl = $market->getFileContents('errors.html');
 } else {
     // Генерируем товары
-    $items = $modx->runSnippet('DocLister', array(
-        'id' => 'items',
+    $items_nopub = $modx->runSnippet('DocLister', array(
+        'id' => 'items_nopub',
         'parents' => 0,
         'showParent' => 1,
         'showNoPublish' => 1,
         'depth' => 10,
         'addWhereList' => 'c.template=' . $template_item,
-        'tvList' => 'vk_item_id,vk_album_id',
-        'tpl' => '@FILE:VKmarket/module_item',
-        'ownerTPL' => '@FILE:VKmarket/module_items_wrap',
+        'filters' => 'AND(tvd:vk_item_id:=:0)',
+        'tvList' => 'vk_item_id',
+        'tpl' => '@FILE:VKmarket/items_nopub',
+        'ownerTPL' => '@FILE:VKmarket/items_nopub_wrap',
         'display' => 12,
         'orderBy' => 'pagetitle ASC',
         'paginate' => 'pages',
@@ -40,22 +41,47 @@ if ($errors !== "") {
         'TplWrapPaginate' => '@CODE:<ul class="pages">[+wrap+]</ul>',
         'TplPrevP' => '',
         'TplNextP' => '',
-        'TplPage' => '@CODE:<li class="pages__item"><a class="pages__link" href="index.php?a=112&amp;id=' . $market->module_id . '[[if? &is=`[+num+]:!=:1` &then=`&amp;items_page=[+num+]`]]">[+num+]</a></li>',
+        'TplPage' => '@CODE:<li class="pages__item"><a class="pages__link" href="index.php?a=112&amp;id=' . $market->module_id . '[[if? &is=`[+num+]:!=:1` &then=`&amp;items_nopub_page=[+num+]`]]">[+num+]</a></li>',
+        'TplCurrentPage' => '@CODE:<li class="pages__item"><span class="pages__link active">[+num+]</span></li>',
+        'TplDotsPage' => '@CODE:<li class="pages__item"><span class="pages__separ">...</span></li>'
+    ));
+
+    $items_pub = $modx->runSnippet('DocLister', array(
+        'id' => 'items_pub',
+        'parents' => 0,
+        'showParent' => 1,
+        'showNoPublish' => 1,
+        'depth' => 10,
+        'addWhereList' => 'c.template=' . $template_item,
+        'filters' => 'AND(tvd:vk_item_id:!=:0)',
+        'tvList' => 'vk_item_id',
+        'tpl' => '@FILE:VKmarket/items_pub',
+        'ownerTPL' => '@FILE:VKmarket/items_pub_wrap',
+        'display' => 12,
+        'orderBy' => 'pagetitle ASC',
+        'paginate' => 'pages',
+        'PrevNextAlwaysShow' => 0,
+        'pageAdjacents' => 4,
+        'TplWrapPaginate' => '@CODE:<ul class="pages">[+wrap+]</ul>',
+        'TplPrevP' => '',
+        'TplNextP' => '',
+        'TplPage' => '@CODE:<li class="pages__item"><a class="pages__link" href="index.php?a=112&amp;id=' . $market->module_id . '[[if? &is=`[+num+]:!=:1` &then=`&amp;items_pub_page=[+num+]`]]">[+num+]</a></li>',
         'TplCurrentPage' => '@CODE:<li class="pages__item"><span class="pages__link active">[+num+]</span></li>',
         'TplDotsPage' => '@CODE:<li class="pages__item"><span class="pages__separ">...</span></li>'
     ));
 
     // Генерируем подборки
-    $albums = $modx->runSnippet('DocLister', array(
-        'id' => 'albums',
+    $albums_nopub = $modx->runSnippet('DocLister', array(
+        'id' => 'albums_nopub',
         'parents' => 0,
         'showParent' => 1,
         'showNoPublish' => 1,
         'depth' => 10,
         'addWhereList' => 'c.template=' . $template_album,
-        'tvList' => 'vk_item_id,vk_album_id',
-        'tpl' => '@FILE:VKmarket/module_album',
-        'ownerTPL' => '@FILE:VKmarket/module_albums_wrap',
+        'filters' => 'AND(tvd:vk_album_id:=:0)',
+        'tvList' => 'vk_album_id',
+        'tpl' => '@FILE:VKmarket/albums_nopub',
+        'ownerTPL' => '@FILE:VKmarket/albums_nopub_wrap',
         'display' => 12,
         'orderBy' => 'pagetitle ASC',
         'paginate' => 'pages',
@@ -64,7 +90,30 @@ if ($errors !== "") {
         'TplWrapPaginate' => '@CODE:<ul class="pages">[+wrap+]</ul>',
         'TplPrevP' => '',
         'TplNextP' => '',
-        'TplPage' => '@CODE:<li class="pages__item"><a class="pages__link" href="index.php?a=112&amp;id=' . $market->module_id . '[[if? &is=`[+num+]:!=:1` &then=`&amp;albums_page=[+num+]`]]">[+num+]</a></li>',
+        'TplPage' => '@CODE:<li class="pages__item"><a class="pages__link" href="index.php?a=112&amp;id=' . $market->module_id . '[[if? &is=`[+num+]:!=:1` &then=`&amp;albums_nopub_page=[+num+]`]]">[+num+]</a></li>',
+        'TplCurrentPage' => '@CODE:<li class="pages__item"><span class="pages__link active">[+num+]</span></li>',
+        'TplDotsPage' => '@CODE:<li class="pages__item"><span class="pages__separ">...</span></li>'
+    ));
+    $albums_pub = $modx->runSnippet('DocLister', array(
+        'id' => 'albums_pub',
+        'parents' => 0,
+        'showParent' => 1,
+        'showNoPublish' => 1,
+        'depth' => 10,
+        'addWhereList' => 'c.template=' . $template_album,
+        'filters' => 'AND(tvd:vk_album_id:!=:0)',
+        'tvList' => 'vk_album_id',
+        'tpl' => '@FILE:VKmarket/albums_pub',
+        'ownerTPL' => '@FILE:VKmarket/albums_pub_wrap',
+        'display' => 12,
+        'orderBy' => 'pagetitle ASC',
+        'paginate' => 'pages',
+        'PrevNextAlwaysShow' => 0,
+        'pageAdjacents' => 4,
+        'TplWrapPaginate' => '@CODE:<ul class="pages">[+wrap+]</ul>',
+        'TplPrevP' => '',
+        'TplNextP' => '',
+        'TplPage' => '@CODE:<li class="pages__item"><a class="pages__link" href="index.php?a=112&amp;id=' . $market->module_id . '[[if? &is=`[+num+]:!=:1` &then=`&amp;albums_pub_page=[+num+]`]]">[+num+]</a></li>',
         'TplCurrentPage' => '@CODE:<li class="pages__item"><span class="pages__link active">[+num+]</span></li>',
         'TplDotsPage' => '@CODE:<li class="pages__item"><span class="pages__separ">...</span></li>'
     ));
@@ -79,8 +128,10 @@ $placeholders = array(
     'module_id' => $market->module_id,
     'module_url' => $market->module_url,
     'jquery_path' => $market->jquery_path,
-    'items' => $items,
-    'albums' => $albums
+    'items_nopub' => $items_nopub,
+    'items_pub' => $items_pub,
+    'albums_nopub' => $albums_nopub,
+    'albums_pub' => $albums_pub
 );
 
 $output = $modx->parseText($tpl, $placeholders);
